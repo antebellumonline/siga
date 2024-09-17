aa5829ae8db2a74cbc806814447e2e4012a022e7
Andre Ventura <ti@asventura.com.br>
2024-09-17 15:05:28 -0300

    Installing Python and Django Dependencies.

--- siga/settings.py
+++ siga/settings.py
@@ -1,19 +1,24 @@
+"""
+Django settings for your_project.
+
+This module contains the settings for the Django project, including
+configuration for the database, installed apps, middleware, and more.
+"""
+
 from decouple import config
 
-# Segurança
-SECRET_KEY = config('SECRET_KEY')
-
-# Banco de Dados
 DATABASES = {
     'default': {
-        'ENGINE': 'mssql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': '1433',
+        'ENGINE': 'sql_server.pyodbc',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT'),
         'OPTIONS': {
             'driver': 'ODBC Driver 17 for SQL Server',
         },
     }
 }
+
+# Outras configurações do Django...
