ea86c41400212c3cc34cecb28d8ed58e191b3cae
Andre Ventura <ti@asventura.com.br>
2024-09-18 15:06:07 -0300

    Ajuestes no aquivo settings.py

--- siga/settings.py
+++ siga/settings.py
@@ -130,10 +130,3 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Definições adicionais
 DEBUG = True  # Defina como False em produção
 ALLOWED_HOSTS = ['*']  # Substitua '*' por domínios específicos em produção
-
-
-print("DB_NAME:", os.getenv('DB_NAME'))
-print("DB_USER:", os.getenv('DB_USER'))
-print("DB_PASSWORD:", os.getenv('DB_PASSWORD'))
-print("DB_HOST:", os.getenv('DB_HOST'))
-print("DB_PORT:", os.getenv('DB_PORT'))
