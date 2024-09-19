e60ab2b17f8104bccccdba7dc792e1cbbb0ab3cc
Andre Ventura <ti@asventura.com.br>
2024-09-19 10:14:44 -0300

    Correções nas Views do Projeto Aluno.

--- test.py
+++ test.py
@@ -1,16 +1,1 @@
-import pyodbc
 
-conn_str = (
-    'DRIVER={ODBC Driver 18 for SQL Server};'
-    'SERVER=siga-database-server.database.windows.net;'
-    'DATABASE=siga-database;'
-    'UID=administrador;'
-    'PWD=p@c3mBellum'
-)
-
-try:
-    conn = pyodbc.connect(conn_str)
-    print("Conexão bem-sucedida!")
-    conn.close()
-except Exception as e:
-    print(f"Erro ao conectar: {e}")
