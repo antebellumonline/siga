6291add739805ac3f379c6609b9dc2b152cf9466
Andre Ventura <ti@asventura.com.br>
2024-09-19 12:47:24 -0300

    Adequação do projeto Cidades

--- localidades/models.py
+++ localidades/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Cidade(models.Model):
+    nome = models.CharField(max_length=100)
+    codigo_ibge = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return f'{self.nome} ({self.codigo_ibge})'
--- siga/settings.py
+++ siga/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'siga',
     'alunos',
+    'localidades',
     # Adicione outros aplicativos aqui
 ]
 
