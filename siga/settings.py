5e3f212e11eda377b58d4984d820037b095504a2
Andre Ventura <ti@asventura.com.br>
2024-09-23 10:52:07 -0300

    Criação do arquivo base.html e ajustes de design visual da aplicação.

--- siga/settings.py
+++ siga/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'fontawesome_free'
+    'fontawesome-free',
     'siga', # Aplicativo principal do projeto
     'cidades', # Aplicativo Cidades
     'alunos', #Aplicativo Alunos
