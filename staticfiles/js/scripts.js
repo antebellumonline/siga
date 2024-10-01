748d8f4c6e4ccb71ab77903710d69884941c586b
Andre Ventura <ti@asventura.com.br>
2024-10-01 14:31:06 -0300

    Melhorias no Font Awesome

--- staticfiles/js/scripts.js
+++ staticfiles/js/scripts.js
@@ -7,3 +7,17 @@ document.addEventListener("DOMContentLoaded", function() {
     //     alert("Botão clicado!");
     // });
 });
+
+$(document).ready(function() {
+    // Máscara CPF
+    $('#cpf').inputmask('999.999.999-99');
+    
+    // Máscara CEP
+    $('#cep').inputmask('99999-999');
+    
+    // Máscara para telefones e celulares
+    $('.telefone').inputmask({
+        mask: ['(99) 9999-9999', '(99) 99999-9999'], 
+        keepStatic: true
+    });
+});
