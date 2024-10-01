e17b2fac904a552265abdb34b27204da86308172
Andre Ventura <ti@asventura.com.br>
2024-10-01 14:16:24 -0300

    Melhorias no Font Awesome

--- siga/settings.py
+++ siga/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'fontawesome_free',
+    'fontawesome',
     'siga',
     'cidades',
     'alunos',
