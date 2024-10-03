96e3502c44b5e5aaf4bb8eddc15e5defe10d17b7
Andre Ventura <ti@asventura.com.br>
2024-10-03 10:50:27 -0300

    Padronização do models.py

--- centroProva/models.py
+++ centroProva/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from certificacoes.models import Certificacao
+from alunos.models import Aluno
 
-# Create your models here.
+class CentroProva(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=255)
+    inativo = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nome
+    
+    class Meta:
+        db_table = 'tb_centroProva'
+    
+class CentroProvaExame(models.Model):
+    id = models.AutoField(primary_key=True)
+    certificacao = models.ForeignKey(Certificacao, on_delete=models.CASCADE) # Referencia a tabela tb_certificacao
+    centro_prova = models.ForeignKey(CentroProva, on_delete=models.CASCADE) # Referencia a tabela tb_centroProva
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE) # Referencia a tabela tb_aluno
+    data = models.DateTimeField() # Data e hora do exame
+    presença = models.BooleanField(default=False) # Campo booleano: True para presente, False para ausente
+    cancelado = models.BooleanField(default=False) # Campo booleano: True para cancelado, False para não cancelado
+    inativo = models.BooleanField(default=False) # Campo booleano: True para inativo, False para ativo
+    def __str__(self):
+        return f"{self.certificacao} - {self.centro_prova} - {self.aluno}"
+    
+    class Meta:
+        db_table = 'tb_centroProva-exames'
