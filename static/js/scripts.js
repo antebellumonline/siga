9f72e9ab77f9e59ef96c26e0a2a9fa98d914b40d
André Ventura <ti@asventura.com.br>
2024-10-25 18:06:42 -0300

    Padronização dos estilos do projeto.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log("Página carregada e scripts prontos para uso!");
 
     // Inicializar o Daterangepicker
-    $('#list-daterange').daterangepicker({
+    $('.daterange').daterangepicker({
         startDate: moment(),
         endDate: moment(),
         locale: {
@@ -27,14 +27,14 @@ document.addEventListener("DOMContentLoaded", function() {
     }, function(start, end) {
         // Não usar autoUpdateInput, então só atualize se precisar
         if (start && end) {
-            $('#list-daterange').val(start.format('DD/MM/YYYY') + ' - ' + end.format('DD/MM/YYYY'));
+            $('.daterange').val(start.format('DD/MM/YYYY') + ' - ' + end.format('DD/MM/YYYY'));
         } else {
-            $('#list-daterange').val(''); // Limpa o campo se necessário
+            $('.daterange').val(''); // Limpa o campo se necessário
         }
     });
 
     // Evento para limpar o campo ao cancelar
-    $('#list-daterange').on('cancel.daterangepicker', function(ev, picker) {
+    $('.daterange').on('cancel.daterangepicker', function(ev, picker) {
         $(this).val('');
     });
 
@@ -114,35 +114,46 @@ var modalFeedback = document.getElementsByClassName("modal-feedback")[0];
 var cancelBtn = document.getElementsByClassName("btn-cancel")[0];
 var spanDelete = document.getElementsByClassName("btn-close-modal")[0];
 
-// Quando o usuário clica no botão Excluir, o modal de confirmação aparece
-$(document).on('click', '.btn-delete', function(event) {
-    event.preventDefault(); // Impede o link de redirecionar
-    var itemPk = $(this).data('pk'); // Obtém o PK do item a ser excluído
-    var modelName = $(this).data('model'); // Obtém o nome do modelo a ser excluído
-    var deleteUrl = '/delete/' + modelName + '/' + itemPk + '/'; // Cria a URL de exclusão com base no modelo e PK
-
-    console.log("PK:", itemPk, "Modelo:", modelName, "URL:", deleteUrl); // Adiciona um log para depuração
-    $('.btn-confirm').data('pk', itemPk); // Armazena o PK no botão de confirmação
-    $('.btn-confirm').data('url', deleteUrl); // Armazena a URL de exclusão no botão de confirmação
-    modalDelete.style.display = "block"; // Mostra o modal de confirmação
-    modalDelete.style.opacity = 1; // Reseta a opacidade ao mostrar
-});
-
-function closeModal(modal) {
-    modal.style.opacity = 0; // Inicia a animação de saída
-    setTimeout(function() {
-        modal.style.display = "none"; // Esconde o modal após a animação
-    }, 300); // Tempo da animação
+// Função para abrir um modal
+function openModal(modal) {
+    console.log('Abrindo modal:', modal); // Log
+    modal.style.display = "block";
+    requestAnimationFrame(function() {
+        modal.style.opacity = "1";
+    });
 }
+
+// Função para fechar um modal
+function closeModal(modal) {
+    console.log('Fechando modal:', modal); // Log
+    modal.style.opacity = "0";
+    setTimeout(function() {
+        modal.style.display = "none";
+    }, 300);
+}
+
+// Quando o usuário clica no botão Excluir
+$(document).on('click', '.btn-delete', function(event) {
+    console.log('Botão Excluir clicado'); // Log
+    event.preventDefault();
+    var itemPk = $(this).data('pk');
+    var modelName = $(this).data('model');
+    var deleteUrl = '/delete/' + modelName + '/' + itemPk + '/';
+
+    console.log("PK:", itemPk, "Modelo:", modelName, "URL:", deleteUrl);
+    $('.btn-confirm').data('pk', itemPk);
+    $('.btn-confirm').data('url', deleteUrl);
+    openModal(modalDelete);
+});
 
 // Evento para o botão de cancelamento do modal de confirmação
 cancelBtn.onclick = function() {
-    closeModal(modalDelete); // Fecha o modal ao clicar no botão cancelar
+    closeModal(modalDelete);
 };
 
 // Evento para o botão de fechar do modal de confirmação
 spanDelete.onclick = function() {
-    closeModal(modalDelete); // Fecha o modal ao clicar no botão fechar
+    closeModal(modalDelete);
 };
 
 // Fecha o modal de confirmação ao clicar fora dele
@@ -154,29 +165,28 @@ window.onclick = function(event) {
 
 // Ação de confirmação da exclusão
 $(document).on('click', '.btn-confirm', function() {
-    var itemPk = $(this).data('pk'); // Obtém o PK do item a ser excluído
-    var deleteUrl = $(this).data('url'); // Usa a URL armazenada no botão de confirmação
+    console.log('Botão Confirmar clicado'); // Log
+    var itemPk = $(this).data('pk');
+    var deleteUrl = $(this).data('url');
 
-    // Realiza a chamada AJAX para excluir o item
     $.ajax({
-        url: deleteUrl, // Usa a URL armazenada no botão de confirmação
-        type: 'POST', // Usar POST ou DELETE conforme sua configuração
+        url: deleteUrl,
+        type: 'POST',
         data: {
             'pk': itemPk,
-            'csrfmiddlewaretoken': $('input[name="csrfmiddlewaretoken"]').val() // Captura o token CSRF do formulário
+            'csrfmiddlewaretoken': $('input[name="csrfmiddlewaretoken"]').val()
         },
         success: function(response) {
+            console.log('Resposta do servidor:', response); // Log
             if (response.success) {
-                // Mostra o feedback de sucesso
                 showFeedback("Item excluído com sucesso.", true);
             } else {
-                // Mostra mensagem de erro
                 showFeedback("Erro ao excluir o item: " + response.error, false);
             }
             closeModal(modalDelete);
         },
         error: function(xhr, status, error) {
-            // Exibe uma mensagem de erro
+            console.error('Erro na chamada AJAX:', error); // Log
             showFeedback("Erro ao excluir o item: " + error, false);
         }
     });
@@ -187,35 +197,30 @@ function showFeedback(message, isSuccess) {
     modalFeedback.style.display = "block";
     modalFeedback.querySelector(".modal-feedback-message").textContent = message;
 
-    // Exibe a contagem regressiva
     var countdownElement = modalFeedback.querySelector(".countdown-message");
-    var countdownTime = 5; // Tempo em segundos para a contagem regressiva
+    var countdownTime = 5;
     countdownElement.textContent = "Este pop-up fechará automaticamente em " + countdownTime + " segundos.";
 
-    // Função para atualizar a contagem regressiva
     var countdownInterval = setInterval(function() {
         countdownTime--;
         countdownElement.textContent = "Este pop-up fechará automaticamente em " + countdownTime + " segundos.";
         
         if (countdownTime <= 0) {
             clearInterval(countdownInterval);
-            closeModal(modalFeedback); // Fecha o modal após a contagem
+            closeModal(modalFeedback);
             if (isSuccess) {
-                // Redireciona para a página anterior após a contagem
                 setTimeout(function() {
-                    window.history.back(); // Retorna à página anterior
-                }, 0); // O redirecionamento imediato após o fechamento
+                    window.history.back();
+                }, 0);
             }
         }
-    }, 1000); // Atualiza a cada segundo
+    }, 1000);
 
-    // Se houver um erro, fecha o modal automaticamente após 3 segundos
     if (!isSuccess) {
-        // Fecha o modal automaticamente após 3 segundos
         setTimeout(function() {
-            clearInterval(countdownInterval); // Limpa o intervalo
-            closeModal(modalFeedback); // Fecha o modal de feedback
-            closeModal(modalDelete); // Fecha o modal de exclusão também
-        }, 5000); // Tempo em milissegundos
+            clearInterval(countdownInterval);
+            closeModal(modalFeedback);
+            closeModal(modalDelete);
+        }, 5000);
     }
 }
