253f6d1e3d4052444ca74bef26c4f52a1624291b
André Ventura <ti@asventura.com.br>
2024-11-01 19:19:00 -0300

    Padronização dos estilos do projeto.

--- generate_docs.py
+++ generate_docs.py
@@ -1,42 +1,63 @@
+"""
+Módulo para gerar uma estrutura de diretórios no formato de árvore
+e salvá-la em um arquivo Markdown. Este módulo é utilizado para
+documentar a estrutura do Projeto SIGA.
+"""
+
 import os
 
 def generate_tree(directory, prefix=''):
     """
-    Gera uma string com a estrutura de diretórios no formato de árvore, ignorando arquivos indesejados.
+    Gera uma string com a estrutura de diretórios no formato de árvore,
+    ignorando arquivos indesejados.
+
+    Args:
+        directory (str): O caminho do diretório a ser explorado.
+        prefix (str): O prefixo utilizado para formatação da árvore.
+
+    Returns:
+        str: Representação em formato de árvore da estrutura de diretórios.
     """
     tree_str = ''
     items = os.listdir(directory)
-    
+
     # Lista de extensões e nomes de arquivos a ignorar
     ignore_files = ['.git', '__pycache__', '.DS_Store', 'venv', 'env', 'node_modules']
-    
+
     for index, item in enumerate(items):
         path = os.path.join(directory, item)
-        
+
         # Ignora diretórios ou arquivos que estão na lista de ignorados
         if item in ignore_files or item.startswith('.'):
             continue
-        
+
         is_last = index == len(items) - 1
         connector = '└── ' if is_last else '├── '
         tree_str += prefix + connector + item + '\n'
-        
+
         if os.path.isdir(path):
             # Chama recursivamente para subdiretórios
             tree_str += generate_tree(path, prefix + ('    ' if is_last else '│   '))
-    
+
     return tree_str
 
 def main():
+    """
+    Função principal que gera a estrutura do projeto Django e a escreve
+    em um arquivo Markdown.
+
+    O diretório do projeto é determinado a partir do caminho relativo
+    do script.
+    """
     # Caminho do diretório do projeto Django
     project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "siga-app"))
-    
+
     # Gera a estrutura da árvore
     project_tree = generate_tree(project_dir)
-    
+
     # Caminho para o arquivo Markdown que será gerado
     markdown_file = os.path.join(project_dir, "structure.md")
-    
+
     # Escreve a estrutura da árvore no arquivo Markdown
     with open(markdown_file, 'w', encoding='utf-8') as md_file:
         md_file.write("# Estrutura do Projeto Django\n\n")
