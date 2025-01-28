2d4242a78cf14f483e6eb7c5186432e138edca54
André Ventura <ti@asventura.com.br>
2025-01-28 12:58:45 -0300

    Ajustes nos relatórios dos app e estilização dos botões de exportação

--- apps/centroProva/views.py
+++ apps/centroProva/views.py
@@ -8,20 +8,15 @@ e interagir com os modelos de dados.
 """
 
 from datetime import datetime
+import openpyxl
 
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
 from django.urls import reverse
 from django.db.models import Q
 
-from reportlab.lib.pagesizes import A4, landscape
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph
-from reportlab.lib import colors
-from reportlab.lib.units import inch
-from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
-from reportlab.pdfgen import canvas
 from django.http import HttpResponse
-import openpyxl
+from reports.utils.pdf_utils import report_create_pdf, Paragraph, ParagraphStyle
 
 from .models import CentroProva, CentroProvaExame, Aluno, Certificacao
 from .forms import CentroProvaForm, CentroProvaExameForm
@@ -363,28 +358,6 @@ def exame_get_filter(request):
 
     return centroprova_exame
 
-class FooterCanvas(canvas.Canvas):
-    def __init__(self, *args, **kwargs):
-        canvas.Canvas.__init__(self, *args, **kwargs)
-        self.pages = []
-
-    def showPage(self):
-        self.pages.append(dict(self.__dict__))
-        self._startPage()
-
-    def save(self):
-        page_count = len(self.pages)
-        for page in self.pages:
-            self.__dict__.update(page)
-            self.draw_footer(page_count)
-            canvas.Canvas.showPage(self)
-        canvas.Canvas.save(self)
-
-    def draw_footer(self, page_count):
-        page = "Página %s de %s" % (self._pageNumber, page_count)
-        self.setFont("Helvetica", 10)
-        self.drawRightString(landscape(A4)[0] - inch, 0.75 * inch, page)
-
 def exame_report_pdf(request):
     """
     View para Gerar Relatório de Exames em PDF com Filtros Aplicados e Personalização
@@ -392,25 +365,10 @@ def exame_report_pdf(request):
     response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = 'attachment; filename="Exames Realizados no Centro de Provas.pdf"'
 
-    # Definir a orientação para paisagem e reduzir margens
-    doc = SimpleDocTemplate(response, pagesize=landscape(A4), 
-                            leftMargin=0.5 * inch, rightMargin=0.5 * inch,
-                            topMargin=0.5 * inch, bottomMargin=0.5 * inch)
-    elements = []
-
-    # Título
-    styles = getSampleStyleSheet()
-    title = Paragraph("Exames Realizados no Centro de Provas", styles['Title'])
-    elements.append(title)
-
-    # Subtítulo com data de geração
-    subtitle = Paragraph(f"Gerado em: {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}", styles['Heading2'])
-    elements.append(subtitle)
-
     # Dados dos exames
     exames = exame_get_filter(request)
     data = [['Data do Exame', 'Centro de Provas', 'Certificação', 'Aluno', 'Presença', 'Cancelado', 'Observação']]
-    
+
     # Estilo para as células da tabela com quebra automática de linha
     cell_style = ParagraphStyle(name='Normal', wordWrap='CJK')
 
@@ -418,7 +376,7 @@ def exame_report_pdf(request):
         certificacao_text = f"{exame.certificacao.descricao} ({exame.certificacao.siglaExame})" if exame.certificacao.siglaExame else exame.certificacao.descricao
         aluno_text = f"{exame.aluno.uid} - {exame.aluno.nome}"
         presenca_text = "Presente" if exame.presenca else "Ausente"
-        
+
         data.append([
             Paragraph(exame.data.strftime('%d/%m/%Y %H:%M:%S'), cell_style),
             Paragraph(str(exame.centroProva), cell_style),
@@ -429,39 +387,7 @@ def exame_report_pdf(request):
             Paragraph(exame.observacao or '', cell_style)
         ])
 
-    # Calcular a largura disponível e ajustar as colunas
-    page_width = landscape(A4)[0] - 1 * inch  # Subtrair margens (ajuste conforme necessário)
-    col_widths = [page_width / len(data[0])] * len(data[0])
-
-    # Criar a tabela com larguras ajustadas para as colunas
-    table = Table(data, colWidths=col_widths)
-
-    # Estilo da tabela
-    table_style = TableStyle([
-        ('BACKGROUND', (0, 0), (-1, 0), colors.HexColor('#E6B510')),  # Cor de fundo do cabeçalho
-        ('TEXTCOLOR', (0, 0), (-1, 0), colors.black),  # Cor do texto do cabeçalho
-        ('ALIGN', (0, 0), (-1, -1), 'LEFT'),  # Alinhar texto à esquerda
-        ('FONTNAME', (0, 0), (-1, -1), 'Helvetica'),  # Fonte da tabela
-        ('FONTSIZE', (0, 0), (-1, -1), 10),  # Tamanho da fonte
-        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),  # Espaçamento inferior no cabeçalho
-        ('VALIGN', (0, 0), (-1, -1), 'TOP')  # Alinhamento vertical no topo
-    ])
-
-    # Adicionar estilo de linhas alternadas
-    for i in range(1, len(data)):
-        if i % 2 == 0:
-            bg_color = colors.HexColor('#e3e3e1')  # Cor de fundo das linhas pares
-        else:
-            bg_color = colors.white  # Cor de fundo das linhas ímpares
-        table_style.add('BACKGROUND', (0, i), (-1, i), bg_color)
-
-    table.setStyle(table_style)
-
-    elements.append(table)
-
-    doc.build(elements, canvasmaker=FooterCanvas)
-
-    return response
+    return report_create_pdf(response, "Exames Realizados no Centro de Provas", data)
 
 def exame_report_xlsx(request):
     """
