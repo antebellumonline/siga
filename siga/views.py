25796b945fad4b70b03a7953dbea972656f69ea4
André Ventura <ti@asventura.com.br>
2025-02-06 14:02:36 -0300

    Corrige status de erro na resposta JSON ao excluir item

--- siga/views.py
+++ siga/views.py
@@ -64,6 +64,10 @@ def delete_item(request, model_name, pk):
             return JsonResponse({'success': True})
         except ImportError as e:
             print(f'Erro ao excluir o item: {e}')
-            return JsonResponse({'success': False, 'error': 'Ocorreu um erro interno ao excluir o item.'}, status=500)
+            return JsonResponse(
+                {'success': False,
+                'error': 'Ocorreu um erro interno ao excluir o item.'},
+                status=50
+            )
     print('Método não permitido.')
     return JsonResponse({'success': False, 'error': 'Método não permitido'}, status=405)
