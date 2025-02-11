749a7f63a24c212880d6c78ab42e8390543b0202
André Ventura <ti@asventura.com.br>
2025-02-11 18:12:14 -0300

    Atualiza a definição das views para o aplicativo 'cursos'.

--- apps/cursos/views.py
+++ apps/cursos/views.py
@@ -1,7 +1,7 @@
 # apps/cursos/views.py
 
 """
-Definição das views para o aplicativo 'centroProva'.
+Definição das views para o aplicativo 'cursos'.
 
 As views utilizam Django para gerenciar as requisições HTTP 
 e interagir com os modelos de dados.
