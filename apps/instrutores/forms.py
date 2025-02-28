4181f05e7d3e3f6b43090d99500eda9d540e0fad
André Ventura <ti@asventura.com.br>
2025-02-28 12:27:07 -0300

    Criação do forms.py no app instrutores

--- apps/instrutores/forms.py
+++ apps/instrutores/forms.py
@@ -4,3 +4,37 @@
 Este módulo contém os formulários utilizados para o aplicativo 'instrutores'.
 Ele define os formulários para cadastro, edição e outras operações relacionadas aos Instrutores.
 """
+
+from django import forms
+from .models import Instrutor
+
+class InstrutorForm(forms.ModelForm):
+    """
+    Formulário para cadastro e edição de Centros de Provas.
+
+    """
+    class Meta:
+        """Configurações meta do formulário"""
+        model = Instrutor
+        fields = [
+            'nome',
+            'observacao',
+            'inativo',
+        ]
+        widgets = {
+            'nome': forms.TextInput(attrs={
+                'class': 'apps-form-input',
+                'id': 'instrutor-nome',
+                'placeholder': 'Digite o nome do Instrutor',
+            }),
+            'observacao': forms.Textarea(attrs={
+                'class': 'apps-form-input',
+                'id': 'instrutor-observacao',
+                'placeholder': 'Escreva alguma observação sobre o Instrutor',
+                'rows': 2,
+            }),
+            'inativo': forms.CheckboxInput(attrs={
+                'class': 'apps-form-input',
+                'id': 'instrutor-inativo',
+            }),
+        }
