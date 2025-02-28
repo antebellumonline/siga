557f4c88d4dc96c51020a6fcf1f355f72df31613
André Ventura <ti@asventura.com.br>
2025-02-28 10:28:16 -0300

    Criação do models.py do app instrutores

--- apps/instrutores/models.py
+++ apps/instrutores/models.py
@@ -6,3 +6,21 @@ Definições dos modelos do aplicativo 'instrutores'.
 Este arquivo contém as classes de modelos usadas para representar e manipular
 os dados relacionados aos Instrutores no banco de dados.
 """
+
+from django.db import models
+
+class Instrutor(models.Model):
+    """
+    Modelo que representa um Instrutor no sistema.
+    """
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=255)
+    observacao = models.TextField(blank=True, null=True)
+    inativo = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.nome)
+
+    class Meta:
+        """Meta-informações para o modelo Instrutor"""
+        db_table = 'tb_instrutor'
