9d86f390e327ad9cccd759f4e7401c495a567501
André Ventura <ti@asventura.com.br>
2025-03-03 16:07:28 -0300

    Ajustes no models do app locais e adição de comentários no fields do app auxiliares

--- apps/auxiliares/fields.py
+++ apps/auxiliares/fields.py
@@ -1,8 +1,16 @@
 # apps/auxiliares/fields.py
 
+"""
+Este módulo contém definições de campos personalizados para o projeto.
+"""
+
 from django.db import models
 
 class InativoField(models.BooleanField):
+    """
+    Um campo BooleanField personalizado que utiliza escolhas
+    específicas para indicar se algo está inativo.
+    """
     def __init__(self, *args, **kwargs):
         inativo_choices = [
             (False, 'Não'),
--- apps/local/models.py
+++ apps/local/models.py
@@ -5,6 +5,7 @@ Definições dos modelos do aplicativo 'local'.
 """
 
 from django.db import models
+from apps.auxiliares.fields import InativoField
 
 class Estado(models.Model) :
     """
@@ -48,7 +49,7 @@ class Local (models.Model):
     bairro = models.CharField(max_length=255, blank=True, null=True)
     cidade = models.ForeignKey(Cidade, on_delete=models.SET_NULL, blank=True, null=True)
     observacao = models.TextField(blank=True, null=True)
-    inativo = models.BooleanField(default=False)
+    inativo = InativoField(default=False)
 
     def __str__(self):
         return str(self.nome)
