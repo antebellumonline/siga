345960f261f374a94fe6cc94d75d63698627251f
André Ventura <ti@asventura.com.br>
2025-03-04 09:38:42 -0300

    Criação dos formulários para cadastro e edição de Empresas e Contatos

---
+++ apps/empresas/forms.py
@@ -0,0 +1,149 @@
+# apps/empresas/forms.py
+
+"""
+Este módulo contém os formulários utilizados para o App Cursos.
+"""
+
+from django import forms
+from django.forms import inlineformset_factory
+
+from .models import Empresa, EmpresaContato, Cidade, ConfigTpContato
+
+class EmpresaForm(forms.ModelForm):
+    """
+    Formulário para cadastro e edição de Empresas.
+    """
+    class Meta:
+        """
+        Configurações meta do formulário.
+        """
+        model = Empresa
+        fields = [
+            'taxId',
+            'razaoSocial',
+            'fantasia',
+            'endereco',
+            'numero',
+            'complemento',
+            'bairro',
+            'cep',
+            'cidade',
+            'observacao',
+            'inativo'
+        ]
+        widgets = {
+            'taxId': forms.TextInput(attrs={
+                'class': 'apps-form-input',
+                'id': 'empresa-taxId',
+                'placeholder': 'Digite o CNPJ ou Tax ID da Empresa',
+            }),
+            'razaoSocial': forms.TextInput(attrs={
+                'class': 'apps-form-input',
+                'id': 'empresa-razaoSocial',
+                'placeholder': 'Digite a Razão Social da Empresa',
+            }),
+            'fantasia': forms.TextInput(attrs={
+                'class': 'apps-form-input',
+                'id': 'empresa-fantasia',
+                'placeholder': 'Digite o Nome Fantasia da Empresa',
+            }),
+            'endereco': forms.TextInput(attrs={
+                'class': 'apps-form-input',
+                'id': 'empresa-endereco',
+                'placeholder': 'Digite o Endereço da Empresa',
+            }),
+            'numero': forms.TextInput(attrs={
+                'class': 'apps-form-input',
+                'id': 'empresa-numero',
+                'placeholder': 'Digite o Número do Endereço',
+            }),
+            'complemento': forms.TextInput(attrs={
+                'class': 'apps-form-input',
+                'id': 'empresa-complemento',
+                'placeholder': 'Digite o Complemento do Endereço',
+            }),
+            'bairro': forms.TextInput(attrs={
+                'class': 'apps-form-input',
+                'id': 'empresa-bairro',
+                'placeholder': 'Digite o Bairro da Empresa',
+            }),
+            'cep': forms.TextInput(attrs={
+                'class': 'apps-form-input',
+                'id': 'empresa-cep',
+                'placeholder': 'Digite o CEP da Empresa',
+            }),
+            'cidade': forms.Select(attrs={
+                'class': 'apps-form-input select2',
+                'id': 'empresa-cidade',
+                'placeholder': 'Digite a Cidade da Empresa',
+            }),
+            'observacao': forms.Textarea(attrs={
+                'class': 'apps-form-input',
+                'id': 'empresa-observacao',
+                'placeholder': 'Escreva alguma observação sobre a Empresa',
+                'rows': 2,
+            }),
+            'inativo': forms.CheckboxInput(attrs={
+                'class': 'apps-form-input',
+                'id': 'empresa-inativo',
+                }),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['cidade'].queryset = Cidade.objects.order_by('nome')
+        self.fields['cidade'].label_from_instance = self.cidade_label_from_instance
+
+    def cidade_label_from_instance(self, obj):
+        """
+    Retorna uma string formatada com o nome da Cidade e o Estado.
+    """
+        return f"{obj.nome} ({obj.estado})"
+
+class EmpresaContatoForm(forms.ModelForm):
+    """
+    Formulário para cadastro e edição de contatos das Empresas.
+    """
+    model = EmpresaContato
+    fields = [
+        'empresa',
+        'tipoContato',
+        'contato',
+        'detalhe'
+    ]
+    widgets = {
+        'empresa': forms.Select(attrs={
+            'class': 'apps-form-input',
+            'id': 'empresaContato-empresa',
+            'placeholder': 'Selecione a Empresa',
+        }),
+        'tipoContato': forms.Select(attrs={
+            'class': 'apps-form-input select2',
+            'id': 'empresaContato-tipoContato',
+            'placeholder': 'Selecione o Tipo de Contato',
+        }),
+        'contato': forms.TextInput(attrs={
+            'class': 'apps-form-input',
+            'id': 'empresaContato-contato',
+            'placeholder': 'Digite o Contato',
+        }),
+        'detalhe': forms.Textarea(attrs={
+            'class': 'apps-form-input',
+            'id': 'empresaContato-detalhe',
+            'placeholder': 'Digite algum detalhe sobre o Contato',
+            'rows': 2,
+        }),
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['tipoContato'].queryset = ConfigTpContato.objects.order_by('descricao')
+
+# FormSet para contatos das Empresas
+EmpresaContatoFormSet = inlineformset_factory(
+    Empresa,
+    EmpresaContato,
+    form=EmpresaContatoForm,
+    extra=4,
+    can_delete=True,
+)
