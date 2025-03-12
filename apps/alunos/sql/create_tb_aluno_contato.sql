a8614c23685ccd88e9fdecacb18fc0eda5534368
André Ventura <ti@asventura.com.br>
2025-03-12 18:04:47 -0300

    Remove os scrips manuais para a criação de tabelas no banco de dados

--- apps/alunos/sql/create_tb_aluno_contato.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE tb_aluno_contato (
-    id INT IDENTITY PRIMARY KEY,                -- Identificador único para o contato
-    uidAluno INT NOT NULL,                     -- Código que relaciona com a tabela tb_aluno
-    codTpContato INT NOT NULL,                 -- Código que relaciona com a tabela tb_config_tpContato
-    contato NVARCHAR(255) NOT NULL,            -- Dados de contato (celular, telefone ou e-mail)
-    detalhe NVARCHAR(MAX),                     -- Observações adicionais
-    FOREIGN KEY (uidAluno) REFERENCES tb_aluno(UID), -- Relacionamento com a tabela tb_aluno
-    FOREIGN KEY (codTpContato) REFERENCES tb_config_tpContato(id) -- Relacionamento com a tabela tb_config_tpContato
-);
